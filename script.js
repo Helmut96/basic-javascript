f03c6c0b3c2131e54e5850f1c3f6f380c65d783e
helmut4646 <elmudfile@gmail.com>
2024-07-10 11:38:09 +0700

    variable, popup box, control flo

--- script.js
+++ script.js
@@ -1,3 +1,62 @@
-var x = 10; 
+/* var x = 10; 
 console.log('Hello World');
-console.log('isi dari variabel x adalah  ' + x)
+console.log('isi dari variabel x adalah  ' + x) */
+
+/* alert('Hello');
+alert('nama saya');
+alert('dani'); */
+
+/* var nama = prompt('masukkan nama:');
+alert(nama); */
+
+/* var test = confirm('kamu yakin');
+if(test === true)
+{
+	 alert('user menekan tombol ok');
+}
+else
+{
+     alert('user menekan tombol cancel')
+} */
+
+
+/* alert('selamat datang');
+var lagi = true;
+
+while(lagi)
+{
+	 var nama = prompt('masukkan nama');
+	 alert('halo '+ nama);
+
+	 lagi = confirm('coba lagi?');
+}
+
+alert('terima kasih..'); */
+
+/* alert('mulai');
+for(var i = 0; i < 5; i++)
+{
+	 alert('Hello World');
+}
+alert('selesai'); */
+
+// penkondisian 
+/*
+var angka = prompt('masukkan angka');
+if(angka % 2 === 0)
+{
+	alert(angka +' adalah bilangan genap');
+}
+else
+{
+	alert(angka +' adalah bilangan ganjil');
+}
+*/
+var ulang = true;
+while(ulang)
+{
+     console.log('hello world');
+     ulang = confirm('lagi?'); 
+}
+
+// console.log('Hello World')
